08e1c98617d5ea82bbaf67441a71b1aa9305526b
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-02-19 19:32:12 +0300

    Update TWorker.cpp

--- Lab2.md/TWorker.cpp
+++ Lab2.md/TWorker.cpp
@@ -3,36 +3,25 @@
 #include "TWorker.h"
 
 TWorker::~TWorker()
-{
-	delete[] FIO;
-}
+{}
 
 TWorker::TWorker()
 {
-	FIO = new char[1];
-	FIO[0] = '\0';
+	FIO = '\0';
 	Scale = 0;
 	Increment = 0;
 }
 
 TWorker::TWorker(const TWorker& worker)
 {
-	int i(0);
-	while (worker.FIO[i] != '\0') i++;
-	FIO = new char[i + 1];
-	memcpy(FIO, worker.FIO, (i + 1) * sizeof(char));
+	FIO = worker.FIO;
 	Scale = worker.Scale;
 	Increment = worker.Increment;
 }
 
 void TWorker::Init(const std::string fio, size_t scale, float increment)
 {
-	delete[] FIO;
-	int i(0);
-	while (fio[i] != '\0') i++;
-	FIO = new char[i + 1];
-	for (i = 0; fio[i] != '\0'; i++) FIO[i] = fio[i];
-	FIO[i] = '\0';
+	FIO = fio;
 	Scale = scale;
 	Increment = increment;
 }
@@ -42,7 +31,7 @@ double TWorker::SalaryCalc() const
 	return (Scale + Scale * Increment);
 }
 
-std::ostream & operator<<(std::ostream &out, const TWorker &worker)
+std::ostream& operator << (std::ostream& out, const TWorker& worker)
 {
 	std::cout << "FIO is "
 		<< worker.FIO
