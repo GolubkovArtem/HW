38816d64744b1af9cb2ac2d20eab6e83ddcf5f43
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-02-19 15:03:49 +0300

    Create TWorker.h

---
+++ Lab2.md/TWorker.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+class TWorker
+{
+private:
+	char* FIO;
+	size_t Scale;
+	float Increment;
+public:
+	~TWorker();
+	TWorker();
+	TWorker(const TWorker& worker);
+	void Init(const std::string fio, size_t scale, float increment);
+	double SalaryCalc() const;
+	friend std::ostream & operator<<(std::ostream &, const TWorker &);
+};
+
+std::ostream & operator<<(std::ostream &out, const TWorker &worker);
