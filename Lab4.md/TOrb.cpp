a7f02d4ab009be4f8915e140cec9adbd4ede90a3
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-02-25 18:44:44 +0300

    Update TOrb.cpp

--- Lab4.md/TOrb.cpp
+++ Lab4.md/TOrb.cpp
@@ -17,10 +17,9 @@ double TOrb::Surface() const
 	return (4. / 3 * M_PI * pow(Radius, 3));
 }
 
-std::ostream& operator << (std::ostream& out, const TOrb& orb)
+void TOrb::Show() const
 {
-	out << "Radius is " << orb.Radius
-		<< " Surface is " << orb.Surface()
-		<< "\n";
-	return out;
+	std::cout << "Radius is " << Radius
+		  << " Surface is " << this->Surface()
+  		  << "\n";
 }
