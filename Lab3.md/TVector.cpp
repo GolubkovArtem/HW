b9541f8efa13d4f6f1308585a580a49a0bc65b26
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-02-25 14:00:50 +0300

    Update TVector.cpp

--- Lab3.md/TVector.cpp
+++ Lab3.md/TVector.cpp
@@ -8,16 +8,15 @@ TVector::~TVector()
 
 TVector::TVector()
 {
-	PVector = new double[1];
-	PVector[0] = 0;
+	PVector = new double[0];
 	Length = 0;
 }
 
 TVector::TVector(const TVector& rhs)
 {
-	PVector = new double[rhs.VectorLength() + 1];
-	memcpy(PVector, rhs.PVector, (rhs.VectorLength() + 1) * sizeof(double));
-	Length = rhs.VectorLength();
+	PVector = new double[rhs.Length];
+	memcpy(PVector, rhs.PVector, rhs.Length * sizeof(double));
+	Length = rhs.Length;
 }
 
 TVector::TVector(const double* vector, int length)
@@ -29,11 +28,12 @@ TVector::TVector(const double* vector, int length)
 
 TVector& TVector::operator = (const TVector& vector)
 {
+	if (PVector == vector.PVector) return (*this);
 	delete[] PVector;
-	PVector = new double[vector.VectorLength()];
-	memcpy(PVector, vector.PVector, (vector.VectorLength() + 1) * sizeof(double));
-	Length = vector.VectorLength();
-	return (*this);
+	PVector = new double[vector.Length];
+	memcpy(PVector, vector.PVector, vector.Length * sizeof(double));
+	Length = vector.Length;
+	return *this;
 }
 
 double TVector::operator [] (int index) const
@@ -41,28 +41,19 @@ double TVector::operator [] (int index) const
 	return PVector[index];
 }
 
-int TVector::VectorLength() const
+TVector operator + (const double* vector1, const TVector& vector2)
 {
-	return Length;
-}
-
-TVector operator + (const double* vector1, const TVector vector2)
-{
-	if (vector1[0] != vector2.VectorLength())
-	{
-		TVector buf0;
-		return buf0;
-	}
-	double* mas = new double[vector2.VectorLength()];
-	for (int i = 0; i < vector2.VectorLength(); i++) mas[i] = vector1[i + 1] + vector2[i];
-	TVector buf(mas, vector2.VectorLength());
-	delete[] mas;
+	TVector buf;
+	if (vector1[0] != vector2.Length) return buf;
+	delete[] buf.PVector;
+	buf.PVector = new double[vector2.Length];
+	for (int i = 0; i < vector2.Length; i++) buf.PVector[i] = vector1[i + 1] + vector2[i];
+	buf.Length = vector2.Length;
 	return buf;
 }
 
 std::ostream& operator << (std::ostream& out, const TVector& vector)
 {
-	int i(0);
-	for (i = 0; i < vector.VectorLength(); i++) out << vector[i];
+	for (int i = 0; i < vector.Length; i++) out << vector[i];
 	return out;
 }
