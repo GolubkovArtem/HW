1d4123c656e3b1a8a4bd8585d9ab446bf90596e2
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-02-25 14:01:24 +0300

    Update TVector.h

--- Lab3.md/TVector.h
+++ Lab3.md/TVector.h
@@ -11,11 +11,11 @@ public:
 	TVector(const TVector& rhs);
 	TVector(const double* vector, int length);
 	double operator [] (int index) const;
-	int VectorLength() const;
 	TVector& operator = (const TVector& vector);
 	friend std::ostream& operator << (std::ostream&, const TVector&);
+	friend TVector operator + (const double* , const TVector& );
 };
+TVector operator + (const double* vector1, const TVector& vector2);
 
-TVector operator + (const double* vector1, const TVector vector2);
 
 std::ostream& operator << (std::ostream& out, const TVector& vector);
