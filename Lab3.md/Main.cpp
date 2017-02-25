0f2547634088cab56648806667d506b4731cd365
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-02-25 14:00:08 +0300

    Update Main.cpp

--- Lab3.md/Main.cpp
+++ Lab3.md/Main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "TVector.h"
 
-int main() 
+int main()
 {
 	TVector vec1;
 	double str1[5] = { 4, 1, 1 ,0, 0 };
 	double str2[4] = { 0, 0, 1, 1 };
 	TVector vec2(str2, 4);
+	vec2 = vec2;
 	vec1 = vec2;
 	TVector vec3 = str1 + vec2;
 	std::cout << vec1 << std::endl;
