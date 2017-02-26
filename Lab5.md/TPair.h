3ce4b654195e5bbc89f6d9028cb1e1273664fb14
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-02-26 10:47:37 +0300

    Create TPair.h

---
+++ Lab5.md/TPair.h
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class TPair
+{
+protected:
+	int First;
+	int Second;
+public:
+	TPair();
+	TPair(const TPair& pair);
+	TPair(int first, int second);
+	virtual void operator () (int first, int second);
+	TPair& operator = (const TPair& pair);
+	virtual TPair operator + (const TPair& pair);
+	friend std::ostream& operator << (std::ostream& , const TPair& );
+};
+
+std::ostream& operator << (std::ostream& out, const TPair& pair);
