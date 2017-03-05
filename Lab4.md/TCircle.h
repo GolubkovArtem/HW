9186933195673d98968b34e56758eeab3875b64e
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-03-05 22:27:31 +0300

    Update TCircle.h

--- Lab4.md/TCircle.h
+++ Lab4.md/TCircle.h
@@ -1,13 +1,23 @@
 #include <iostream>
-#define M_PI 3.1415926535
+extern float M_PI(3.1415926535);
 
 class TCircle
 {
 protected:
 	size_t Radius;
 public:
-	TCircle();
-	TCircle(size_t radius);
-	virtual double Surface() const;
-	virtual void Show() const;
+	TCircle() :Radius(0)
+	{}
+	TCircle(size_t radius) :Radius(radius)
+	{}
+	virtual double Surface() const
+	{
+		return (M_PI * pow(Radius, 2));
+	}
+	virtual void Show() const
+	{
+		std::cout << "Radius is " << Radius
+			<< " Surface is " << this->Surface()
+			<< std::endl;
+	}
 };
