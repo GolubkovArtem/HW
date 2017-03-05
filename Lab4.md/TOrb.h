81a8a961fffa34ef4ca8c5ad185110789151ccbc
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-03-05 22:27:08 +0300

    Update TOrb.h

--- Lab4.md/TOrb.h
+++ Lab4.md/TOrb.h
@@ -1,11 +1,22 @@
-#include <iostream>
 #include "TCircle.h"
+extern float M_PI;
 
-class TOrb: public TCircle
+class TOrb : public TCircle
 {
 public:
-	TOrb();
-	TOrb(size_t radius);
-	double Surface() const;
-	void Show() const;
+	TOrb() :TCircle()
+	{}
+	TOrb(size_t radius) :TCircle(radius)
+	{}
+	double Surface() const
+	{
+		return (4. / 3 * M_PI * pow(Radius, 3));
+	}
+
+	void Show() const
+	{
+		std::cout << "Radius is " << Radius
+			<< " Surface is " << this->Surface()
+			<< std::endl;
+	}
 };
