cea82772d9051989b7ce98da7b1257ed6c36d5f7
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-04-16 12:56:27 +0300

    Create TSet.h

---
+++ HW1/TSet.h
@@ -0,0 +1,450 @@
+#include <iostream>
+#include <vector>
+
+
+
+
+template <class T> 
+struct less 
+{
+	bool operator() (const T & x, const T & y) const {
+		return x < y;
+	}
+};
+
+
+
+//////////////////////////////////////////////////// struct bidirectional_iterator
+template <class T>
+struct bidirectional_iterator
+{
+
+	using value_type = T;
+	using iterator = bidirectional_iterator<T>;
+	using const_iterator = const bidirectional_iterator<T>;
+
+private:
+
+	value_type Value;
+	iterator * Up;
+	iterator * Right;
+	iterator * Left;
+
+public:
+
+	~bidirectional_iterator() = default;
+
+	bidirectional_iterator() 
+		: Up(nullptr)
+		, Right(nullptr)
+		, Left(nullptr)
+	{ }
+
+	bidirectional_iterator(const bidirectional_iterator<T> & bIter)
+		: Value(bIter.Value)
+		, Up(bIter.Up)
+		, Right(bIter.Right)
+		, Left(bIter.Left)
+	{ }
+
+	const bidirectional_iterator & operator = (const bidirectional_iterator<T> & bIter) {
+		Value = bIter.Value;
+		Up = bIter.Up;
+		Right = bIter.Right;
+		Left = bIter.Left;
+		return *this;
+	}
+
+
+	bool operator == (const bidirectional_iterator<T> & bIter) const {
+		return **this == *bIter;
+	}
+
+	bool operator != (const bidirectional_iterator<T> & bIter) const {
+		return **this != *bIter;
+	}
+
+	value_type operator * () const {
+		return Value;
+	}
+
+
+	const iterator & operator ++ () {
+
+		iterator buf(*this);
+
+		if (Right != nullptr) {
+			*this = *Right;
+			while (Left != nullptr) 
+				*this = *Left;
+		} 
+		else if (Up != nullptr && Up->Left != nullptr && **Up->Left == **this)
+			*this = *Up;
+		else {
+			while (Up != nullptr) {
+				*this = *Up;
+				if (Up != nullptr && Up->Left != nullptr && **Up->Left == **this) {
+					*this = *Up;
+					break;
+				}
+				if (Up == nullptr) {
+					*this = buf;
+					return buf;
+				}
+			}
+		}
+
+		return *this;
+	}
+
+	iterator operator ++ (int) {
+		iterator buf(*this);
+		++*this;
+		return buf;
+	}
+
+	const iterator & operator -- () {
+		
+		iterator buf(*this);
+
+		if (Left != nullptr)
+		{
+			*this = *Left;
+			while (Right != nullptr)
+				*this = *Right;
+		}
+		else if (Up != nullptr && Up->Right != nullptr && **Up->Right == **this)
+			*this = *Up;
+		else {
+			while (Up != nullptr) {
+				*this = *Up;
+				if (Up != nullptr && Up->Right != nullptr && **Up->Right == **this) {
+					*this = *Up;
+					break;
+				}
+				if (Up == nullptr) {
+					*this = buf;
+					return buf;
+				}
+			}
+		}
+
+		return *this;
+	}
+
+	iterator operator -- (int) {
+		iterator buf(*this);
+		--*this;
+		return buf;
+	}
+
+
+	template<class T, class Compare = less<T>>
+	friend class TSet;
+};
+
+
+//////////////////////////////////////////////////// class TSet
+template<class T, class Compare = less<T>>
+class TSet
+{
+
+public:
+
+	using value_type = T;
+	using value_compare = Compare;
+	using reference = value_type &;
+	using const_reference = const value_type &;
+	using iterator = bidirectional_iterator<T>;
+	using const_iterator = const bidirectional_iterator<T>;
+	using size_type = size_t;
+
+private:
+
+	iterator * Root;
+	iterator Begin;
+	iterator End;
+	size_type Size;
+
+public:
+
+	~TSet() {
+		clear();
+	}
+
+	TSet()
+		: Root(nullptr)
+		, Size(0)
+	{ }
+
+	TSet(const_reference value) {
+		iterator * newRoot = new iterator;
+		Root = newRoot;
+		Root->Value = value;
+		Begin = *Root;
+		End = *Root;
+		Size = 1;
+	}
+
+	TSet(const TSet & set) {
+		*this = set;
+	}
+
+	TSet & operator = (const TSet & set) {
+		if (!empty())
+			clear();
+		if (set.empty())
+			return *this;		
+		for (auto it = *set.Root; it != set.End; ++it)
+			insert(*it);
+		for (auto it = *set.Root; it != set.Begin; --it)
+			insert(*it);
+		insert(*set.Begin);
+		insert(*set.End);
+		return *this;
+	}
+
+	//////////////////////////////////////////////////// insert(), erase(), swap(), clear()
+	void insert(const_reference value) {
+
+		if (empty()) {
+			iterator * newRoot = new iterator;
+			Root = newRoot;
+			Root->Value = value;
+			Begin = *Root;
+			End = *Root;
+			Size = 1;
+			return;
+		}
+
+		Compare compare;
+		iterator * i = Root;
+
+		while(value != **i) {
+
+			if (compare(**i, value) && i->Right == nullptr) {
+				iterator * newBranch = new iterator;
+				newBranch->Up = i;
+				newBranch->Value = value;
+				i->Right = newBranch;
+				++Size;
+				i = newBranch;
+			}
+			else if (compare(value, **i) && i->Left == nullptr) {
+				iterator * newBranch = new iterator;
+				newBranch->Up = i;
+				newBranch->Value = value;
+				i->Left = newBranch;
+				++Size;
+				i = newBranch;
+			}
+			else if (compare(**i, value))
+				i = i->Right;
+			else
+				i = i->Left;
+
+		}
+
+		this->reIt();
+
+	}
+
+	void erase(const_reference value) {
+
+		if (!count(value))
+			return;
+
+		iterator buf(find(value));
+		iterator * pBuf(nullptr);
+
+		if (value == **Root) {
+
+			if (buf.Left != nullptr && buf.Right == nullptr) {
+				Root = buf.Left;
+				pBuf = Root->Up;
+				Root->Up = nullptr;
+				this->reIt();
+			}
+			else if (buf.Right != nullptr)
+			{
+				Root = buf.Right;
+				pBuf = Root->Up;
+				Root->Up = nullptr;
+				this->reIt();
+				if (buf.Left != nullptr) {
+					this->insertBranch(buf.Left);
+				}
+			}
+
+			if (pBuf == nullptr) {
+				delete Root;
+				Root = nullptr;
+			}
+			else 
+				delete pBuf;
+
+			--Size;
+
+			return;
+		}
+
+
+		if (buf.Up->Left != nullptr && **buf.Up->Left == *buf) {
+			pBuf = buf.Up->Left;
+			buf.Up->Left = nullptr;
+		}
+		else {
+			pBuf = buf.Up->Right;
+			buf.Up->Right = nullptr;
+		}
+
+		this->reIt();
+
+		if (buf.Left != nullptr)
+			this->insertBranch(buf.Left);
+		if (buf.Right != nullptr) 
+			this->insertBranch(buf.Right);
+
+		--Size;
+		delete pBuf;
+
+	}
+
+	void swap(TSet & set) {
+		TSet buf(set);
+		set = *this;
+		*this = buf;
+	}
+
+	void clear() {
+		if (empty())
+			return;
+		for (auto it = Begin; it != End;) {
+			this->erase(*it++);
+			it = find(*it);
+		}
+		this->erase(*End);
+	}
+
+	//////////////////////////////////////////////////// value_compare()
+	const value_compare & value_comp() const {
+		Compare compare;
+		return compare;
+	}
+
+	//////////////////////////////////////////////////// begin(), cbegin(), end(), cend()
+	iterator begin() const {
+		if (empty())
+			throw std::exception("appeal to empty tree");
+		return Begin;
+	}
+
+	const_iterator cbegin() const {
+		if (empty())
+			throw std::exception("appeal to empty tree");
+		return Begin;
+	}
+
+	iterator end() const {
+		if (empty())
+			throw std::exception("appeal to empty tree");
+		return End;
+	}
+
+	const_iterator cend() const {
+		if (empty())
+			throw std::exception("appeal to empty tree");
+		return End;
+	}
+
+
+	size_type empty() const {
+		return !Size;
+	}
+
+	size_type size() const {
+		return Size;
+	}
+
+	//////////////////////////////////////////////////// find(), count()
+	iterator find(const_reference value) const {
+
+		if (empty())
+			throw std::exception("appeal to empty tree");
+
+		iterator it(*Root);
+		Compare compare;
+
+		while (1) {
+			if (*it == value)
+				return it;
+			else if (compare(*it, value) && it.Right != nullptr)
+				it = *it.Right;
+			else if (compare(value, *it) && it.Left != nullptr)
+				it = *it.Left;
+			else
+				return End;
+		}
+
+	}
+
+
+	size_type count(const_reference value) const {
+		return (*find(value) == value);
+	}
+
+	// reIt(), insertBranch() private функции, используемые только в данном классе (в функциях insert, erase())
+	private:
+	
+	// переопределяет Begin и End
+	void reIt() {
+		iterator * it = Root;
+		while (it->Left != nullptr)
+			it = it->Left;
+		Begin = *it;
+		it = Root;
+		while (it->Right != nullptr)
+			it = it->Right;
+		End = *it;
+	}
+
+	// добавляет ветку в дерево без учёта Size
+	void insertBranch(iterator * branch) {
+
+		iterator * it(Root);
+		Compare compare;
+
+		branch->Up = nullptr;
+
+		while (branch->Up == nullptr) {
+			if (compare(**it, **branch) && it->Right != nullptr)
+				it = it->Right;
+			else if (compare(**branch, **it) && it->Left != nullptr)
+				it = it->Left;
+			else if (compare(**it, **branch)) {
+				it->Right = branch;
+				branch->Up = it;
+			}
+			else {
+				it->Left = branch;
+				branch->Up = it;
+			}
+
+		}
+
+		this->reIt();
+
+	}
+
+};
+
+
+template <typename T>
+std::ostream & operator << (std::ostream & out, TSet<T> & set) {
+	if (set.empty())
+		return out;
+	for (auto it = set.begin(); it != set.end(); ++it)
+		out << *it << " ";
+	out << *set.end();
+	return out;
+}
