22ff0644f6636f5d9ed43cb93332bb1b157e5c54
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-05-21 18:50:33 +0300

    Update Main.cpp

--- M3/Qsort/Main.cpp
+++ M3/Qsort/Main.cpp
@@ -8,8 +8,8 @@ void qsort(It beg, It end, Cmp cmp) {
 	for (It i = beg; i != end; ++i)
 		if (cmp(*i, *el)) {
 			for (It j = i; j != el;) {
-				std::iter_swap(j, j - 1); // std::iter_swap(j, --j) не работает, т.к выполняется сначала --j
-				--j;                      // странно я читал, что такое поведение бывает только у постфикса
+				It k = j;      // std::iter_swap(j, --j) не работает, т.к выполняется сначала --j
+				std::iter_swap(k, --j); // странно я читал, что такое поведение бывает только у постфикса
 			}
 			++el;
 		}
